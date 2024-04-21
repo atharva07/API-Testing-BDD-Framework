bf5894d1d48a292ab499f516f1b5e47164c089ed
atharva07 <atharva.hiwase07@gmail.com>
2024-04-22 02:33:12 +0530

    Cucumber reports integrated successfully

--- cucumber/src/test/java/cucumberOptions/TestRunner.java
+++ cucumber/src/test/java/cucumberOptions/TestRunner.java
@@ -7,9 +7,12 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
     features = "src/test/java/features",
+    plugin = "json:target/jsonReports/cucumber-report.json",
     glue = "stepDefinations"
     //tags = "@DeletePlace"
 )
 public class TestRunner {
-    
+    // to integrate cucumber reports in your code, go to the link below and copy POM.xml code and add version as mentioned above
+    // https://github.com/damianszczepanik/maven-cucumber-reporting
+    // If we have multiple feature files, then add the feature file name too in cucumberOptions fearture path
 }
